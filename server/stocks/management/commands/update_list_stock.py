5370078031d1dd6fcccccbdae128d0415b6238ed
aminhp93 <minhpn.org.ec@gmail.com>
2021-04-07 09:53:30 +0700

    Update: Add sqlite

--- server/stocks/management/commands/update_list_stock.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('Start update list stock'))
-        self.stdout.write(self.style.SUCCESS('End update list stock'))
