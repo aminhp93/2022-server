6763c7c2a453620de64bd47bb29acd21c55b1d63
aminhp93 <minhpn.org.ec@gmail.com>
2021-04-07 08:37:17 +0700

    Update: add sqlite

--- server/server/settings.py
+++ server/server/settings.py
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '2022-server',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': '2022-server',
     }
 }
 
